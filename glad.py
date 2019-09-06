4124118b820d6c8380ab20c2fe069ab83f898e34
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2019-09-06 09:18:26 -0300

    adapt code to new logger

--- glad.py
+++ glad.py
@@ -29,8 +29,18 @@ CONFIG = {
     'saveTo': 'drive'
 }
 
+HEADER = """Config file:
+
+{}
+
+Run command:
+
+    {}
+"""
+
+
 # HELPERS
-def check_config(name):
+def load_config(name):
     exists = os.path.isfile(name)
     if exists:
         # load config file
@@ -44,14 +54,19 @@ def check_config(name):
     return config
 
 
-def initEE():
+def initEE(logger=None):
     import ee
     try:
         ee.Initialize()
     except Exception as e:
-        print("Couldn't connect to Earth Engine."
-              " Check your internet connection")
+        msg = "Couldn't connect to Earth Engine. Check your internet connection - {}".format(e)
+        print(msg)
+        if logger:
+            logger.log(msg)
         raise e
+    else:
+        if logger:
+            logger.log('Earth Engine initialized successfully')
 
 
 @click.group()
@@ -162,7 +177,7 @@ def sites():
     """ Show available site names """
     import ee
     initEE()
-    config = check_config('config.json')
+    config = load_config('config.json')
     if not config: return None
 
     siteAP = config['site']['assetPath']
@@ -178,55 +193,95 @@ def sites():
 @click.option('-c', '--clas', default=None, help='The class to export. Can be "probable", "confirmed" or "both"')
 @click.option('-d', '--date', default=None, help='If this param is not set, it will use the date for today')
 @click.option('--site', default=None, help='The name of the site to process, must be present in the parsed property')
-@click.option('-m', '--mask', default=None, help='The mask to apply to the final result. Can be None, "raster" or "vector"')
+@click.option('-m', '--mask', default=True, type=bool, help='Whether to use the mask in config file or not')
 @click.option('-v', '--verbose', default=True, type=bool)
-def alert(savein, verbose, clas, date, site, mask):
+@click.option('--config', default=None, help='The name of the configuration file. Defaults to "config.json"')
+def alert(savein, clas, date, site, mask, verbose, config):
     """ Export GLAD alerts to Google Drive, Earth Engine Asset or Local files.
     Takes configuration parameters from `config.json`.
     """
-    config = check_config('config.json')
+    # LOAD CONFIG FILE
+    configname = config  # change variable name
+    if not configname:
+        configname = 'config.json'
+
+    config = load_config(configname)
     if not config: return None
 
-    import ee
-    initEE()
-    import geepyGLAD as glad
-
-    destination = savein or config['saveTo']
-
-    soptions = ['drive', 'asset', 'local']
-
-    if destination not in soptions:
-        return 'savein parameter must be one of {}'.format(soptions)
-
-    save_params = config[destination]
-
-    # change variable name
-    usersite = site
-
-    # SITE
+    # SITE PARAMS
     site_params = config['site']
     asset_path = site_params['assetPath']
     property_name = site_params['propertyName']
+    usersite = site  # change variable name
+
+    # SAVE PARAMS
+    destination = savein or config['saveTo']
+    save_params = config[destination]
+    soptions = ['drive', 'asset', 'local']
+
+    # DATE PARAMS
+    if not date:
+        date = config['date']
+    alert_date = dt.today().isoformat() if date == 'today' else date
+
+    # MIN AREA
+    limit = config['minArea']
+
+    # CLASS
+    if not clas:
+        clas = config['class']
+
+    # RUN COMMAND AND HASH
+    command = 'glad alert -s {} -c {} -d {} -m {} -v {}'.format(savein, clas, date, mask, verbose)
+    if usersite:
+        command += ' --site {}'.format(usersite)
+
+    config_str = json.dumps(config, indent=2)
+    tohash = '{} {}'.format(config_str, command)
+    tohash = tohash.encode('utf-8')
+    import hashlib
+    h = hashlib.sha256()
+    h.update(tohash)
+    hexcode = h.hexdigest()
+    logname = '{} {}'.format(date, hexcode)
+
+    header = HEADER.format(config_str, command)
+
+    # LOGGER
+    from geepyGLAD.logger import Logger
+    logdir = 'logs'
+    logger = Logger(logname, logdir)
+
+    logger.header(header)
+
+    if destination not in soptions:
+        msg = 'savein parameter must be one of {}'.format(soptions)
+        logger.log(msg)
+        print(msg)
+        return None
+
+    # INITIALIZE EE
+    import ee
+    initEE(logger)
+    try:
+        from geepyGLAD import utils, alerts, batch
+    except Exception as e:
+        msg = 'ERROR while importing geepyGLAD - {}'.format(e)
+        logger.log(msg)
+        raise e
 
     site = ee.FeatureCollection(asset_path)
 
     if usersite:
         site = site.filterMetadata(property_name, 'equals', usersite)
-
-    if not date:
-        date = config['date']
-
-    alert_date = dt.today().isoformat() if date == 'today' else date
-
-    limit = config['minArea']
-
-    if not clas:
-        clas = config['class']
+        site = ee.Feature(site.first())
 
     # Check for available alert image in the given date
-    has_images = glad.utils.has_image(alert_date, glad.alerts.ALERTS).getInfo()
+    has_images = utils.has_image(alert_date, alerts.ALERTS).getInfo()
     if not has_images:
-        print('GLAD alerts not available for date {}'.format(date))
+        msg = 'GLAD alerts not available for date {}'.format(date)
+        logger.log(msg)
+        print(msg)
         return None
 
     args = dict(
@@ -234,59 +289,32 @@ def alert(savein, verbose, clas, date, site, mask):
         date=alert_date,
         clas=clas,
         limit=limit,
-        # smooth=smooth,
         property_name=property_name,
         verbose=verbose,
         folder=save_params['folder'],
+        logger=logger
     )
-
-    # CREATE LOG FILE
-    logname = '{}{}_{}_{}_{}m2.txt'.format(
-        '_'.join(asset_path.split('/')[2:]),
-        '_'+property_name or '_',
-        clas,
-        alert_date,
-        limit
-    )
-    logdir = 'logs'
-
-    if not os.path.isdir(logdir):
-        os.mkdir(logdir)
-    logpath = os.path.join(logdir, logname)
-
-    if os.path.isfile(logpath):
-        perm = 'a'
-    else:
-        perm = 'w+'
-
-    # MASK
-    vector_mask_id = config['vectorMask']
-    if vector_mask_id:
-        vector_mask = ee.FeatureCollection(vector_mask_id)
-        args['vector_mask'] = mask or vector_mask
 
     raster_mask_id = config['rasterMask']
-    if raster_mask_id:
+    if raster_mask_id and mask:
         raster_mask = ee.Image(raster_mask_id)
-        args['raster_mask'] = mask or raster_mask
+        args['raster_mask'] = raster_mask
 
     # COMPUTE ALERTS
     try:
         if destination == 'drive':
-            msgs = glad.batch.toDrive(**args)
-        if destination == 'asset':
-            msgs = glad.batch.toAsset(**args)
-        if destination == 'local':
+            batch.toDrive(**args)
+        elif destination == 'asset':
+            batch.toAsset(**args)
+        elif destination == 'local':
             args['subfolders'] = save_params['subfolders']
-            msgs = glad.batch.toLocal(**args)
+            batch.toLocal(**args)
+        else:
+            raise ValueError('destination {} not allowed'.format(destination))
     except Exception as e:
+        msg = 'ERROR: {}'.format(str(e))
+        logger.log(msg)
         raise e
-        msgs = [str(e)]
-
-    # write log
-    with open(logpath, perm) as log:
-        finalmsg = '\n'.join(msgs)
-        log.write(finalmsg)
 
 
 if __name__ == '__main__':
